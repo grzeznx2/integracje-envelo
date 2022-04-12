69ae4671b36f9af820c92e98dd439e6b972af296
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 16:01:35 +0200

    start working on event props

--- src/app/events/event-item/event-item.component.ts
+++ src/app/events/event-item/event-item.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-event-item',
   templateUrl: './event-item.component.html',
-  styleUrls: ['./event-item.component.scss']
+  styleUrls: ['./event-item.component.scss'],
 })
 export class EventItemComponent implements OnInit {
+  @Input() name!: string;
+  @Input() address!: string;
+  @Input() startDate!: string;
+  @Input() startTime!: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
