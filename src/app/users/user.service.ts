33b6a5dc06244681344fc145acaa8936b4342667
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 21:09:29 +0200

    fetch all users in user service

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from './models/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private users$ = new BehaviorSubject<User[]>([]);
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  get users(): Observable<User[]> {
+    return this.users$;
+  }
+
+  public init() {
+    this.getUsers();
+  }
+
+  public getUsers() {
+    this.http
+      .get<User[]>('http://localhost:3000/users')
+      .pipe(tap(console.log))
+      .subscribe((users) => this.users$.next(users));
+  }
 }
