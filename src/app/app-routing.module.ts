3f3589d160bf9fb84f6b474b63fb0ef4b4ba515d
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 18:55:49 +0200

    implement navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventListComponent } from './events/event-list/event-list.component';
+import { MyEventsComponent } from './events/my-events/my-events.component';
+import { PastEventsComponent } from './events/past-events/past-events.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'invitations',
+    component: EventListComponent,
+  },
+  {
+    path: 'my-events',
+    component: MyEventsComponent,
+  },
+  {
+    path: 'past-events',
+    component: PastEventsComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/invitations',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
