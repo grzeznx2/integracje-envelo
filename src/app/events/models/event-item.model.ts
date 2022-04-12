597433cf86b74b5ce698e10c83fe2aa7837f6451
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 17:53:09 +0200

    add decision type

--- src/app/events/models/event-item.model.ts
+++ src/app/events/models/event-item.model.ts
@@ -1,7 +1,10 @@
+export type Decision = 'YES' | 'NO' | 'MAYBE' | 'NOT DECIDED';
+
 export interface EventItem {
   id: number;
   name: string;
   startDate: Date;
   startTime: Date;
   address: string;
+  decision: Decision;
 }
