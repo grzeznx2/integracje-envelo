b8fbb2d731b9f34b4051dc64f0efd8945b378473
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-13 01:42:51 +0200

    create method for fetching groups

--- src/app/groups/group.service.ts
+++ src/app/groups/group.service.ts
@@ -1,16 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Group } from './models/group.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GroupService {
+  private groups$ = new BehaviorSubject<Group[]>([]);
+
   constructor(private http: HttpClient) {}
+
+  get groups(): Observable<Group[]> {
+    return this.groups$;
+  }
 
   public createGroup(group: Group) {
     this.http
       .post('http://localhost:3000/groups', group)
       .subscribe(console.log);
+  }
+
+  public getGroups() {
+    this.http
+      .get<Group[]>('http://localhost:3000/groups')
+      .subscribe((groups) => this.groups$.next(groups));
   }
 }
