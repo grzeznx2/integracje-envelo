1b3d8f709c2717ac7103cfdcdf0dcd4013ed6f30
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 15:57:45 +0200

    create empty event-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EventItemComponent } from './events/event-item/event-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, EventItemComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
