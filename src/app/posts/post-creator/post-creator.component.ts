2ed08aa97ce9c758678544e022a994ef9ae97253
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-13 00:51:28 +0200

    display post form

--- src/app/posts/post-creator/post-creator.component.ts
+++ src/app/posts/post-creator/post-creator.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-creator',
   templateUrl: './post-creator.component.html',
-  styleUrls: ['./post-creator.component.scss']
+  styleUrls: ['./post-creator.component.scss'],
 })
 export class PostCreatorComponent implements OnInit {
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this._initializeForm();
   }
 
+  private _initializeForm() {
+    this.form = this.fb.group({
+      description: ['', [Validators.required]],
+    });
+  }
 }
