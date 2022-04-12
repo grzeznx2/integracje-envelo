75dd9e1ba6143806764c0af946a06dba9d33eec7
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 20:55:38 +0200

    initialize form

--- src/app/groups/group-creator/group-creator.component.ts
+++ src/app/groups/group-creator/group-creator.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-group-creator',
   templateUrl: './group-creator.component.html',
-  styleUrls: ['./group-creator.component.scss']
+  styleUrls: ['./group-creator.component.scss'],
 })
 export class GroupCreatorComponent implements OnInit {
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this._createForm();
   }
 
+  private _createForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+    });
+  }
 }
