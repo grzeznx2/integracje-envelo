e832bc18606ebc2eeb1a1edd9f343b8a353e5240
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 23:04:58 +0200

    add EventPlace interface

--- src/app/events/models/event-details.model.ts
+++ src/app/events/models/event-details.model.ts
@@ -1,1 +1,11 @@
 export type EventStatus = 'ACTIVE' | 'CANCELLED' | 'FINISHED';
+
+export interface EventPlace {
+  id: number;
+  // placeType: PlaceType
+  city: string;
+  street: string;
+  streetNumber: string;
+  localNumber: string;
+  postcode: string;
+}
