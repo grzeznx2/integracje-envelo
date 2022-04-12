42dc6f05b0bc63ed38bf7ae344be980315a84887
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 23:15:12 +0200

    get event id from url params

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
-  styleUrls: ['./event-details.component.scss']
+  styleUrls: ['./event-details.component.scss'],
 })
 export class EventDetailsComponent implements OnInit {
+  public eventId!: number;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.eventId = Number(this.route.snapshot.paramMap.get('id'));
   }
-
 }
