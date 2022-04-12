31cece72ae142f7a947a8fefab21a5de4f47e58a
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 23:23:26 +0200

    get event by id

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { EventService } from '../event.service';
+import { EventDetails } from '../models/event-details.model';
 
 @Component({
   selector: 'app-event-details',
@@ -8,10 +11,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventDetailsComponent implements OnInit {
   public eventId!: number;
+  public event$: Observable<EventDetails | null> = of(null);
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private eventService: EventService
+  ) {}
 
   ngOnInit(): void {
     this.eventId = Number(this.route.snapshot.paramMap.get('id'));
+    this.eventService.getEventById(this.eventId);
+    this.event$ = this.eventService.event;
   }
 }
