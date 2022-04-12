67372ed56d69d09cdb3ffb8709e212e10628f551
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 19:43:31 +0200

    add name field

--- src/app/events/creator/creator.component.ts
+++ src/app/events/creator/creator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-creator',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creator.component.scss'],
 })
 export class CreatorComponent implements OnInit {
-  constructor() {}
+  public firstForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this._createFirstForm();
+  }
+
+  private _createFirstForm() {
+    this.firstForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      startDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+    });
+  }
 }
