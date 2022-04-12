4128c2183749a9cb2e9b4eec9e22102b19c58ff9
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-12 17:19:17 +0200

    cleanup

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -3,30 +3,6 @@ import { Observable, of } from 'rxjs';
 import { EventService } from '../event.service';
 import { EventItem } from '../models/event-item.model';
 
-const events: EventItem[] = [
-  {
-    name: 'Spotkanie',
-    address: 'Sosnowa 50',
-    startDate: new Date(),
-    startTime: new Date(),
-    id: 1,
-  },
-  {
-    name: 'Spotkanie',
-    address: 'Sosnowa 50',
-    startDate: new Date(),
-    startTime: new Date(),
-    id: 1,
-  },
-  {
-    name: 'Spotkanie',
-    address: 'Sosnowa 50',
-    startDate: new Date(),
-    startTime: new Date(),
-    id: 1,
-  },
-];
-
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
