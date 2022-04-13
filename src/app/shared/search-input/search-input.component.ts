b62c72f3bd531201b65d313465b43ce3faa61e6c
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-13 02:12:18 +0200

    display search input options

--- src/app/shared/search-input/search-input.component.ts
+++ src/app/shared/search-input/search-input.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
-  styleUrls: ['./search-input.component.scss']
+  styleUrls: ['./search-input.component.scss'],
 })
 export class SearchInputComponent implements OnInit {
+  @Input() data!: any[];
+  @Input() field!: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
