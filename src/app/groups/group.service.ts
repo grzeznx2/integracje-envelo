74ef4a8252bb16ff73c7231ba059db0461d7326d
Grzegorz Kubik <grzeznx2@gmail.com>
2022-04-13 08:03:08 +0200

    redirect after createing new group

--- src/app/groups/group.service.ts
+++ src/app/groups/group.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Group } from './models/group.model';
@@ -10,7 +11,11 @@ import { Group } from './models/group.model';
 export class GroupService {
   private groups$ = new BehaviorSubject<Group[]>([]);
 
-  constructor(private http: HttpClient, private toastr: ToastrService) {}
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
   get groups(): Observable<Group[]> {
     return this.groups$;
@@ -23,6 +28,7 @@ export class GroupService {
   public createGroup(group: Group) {
     this.http.post('http://localhost:3000/groups', group).subscribe(() => {
       this.toastr.success('Pomyślnie dodano nową grupę');
+      this.router.navigate(['invitations']);
     });
   }
 
